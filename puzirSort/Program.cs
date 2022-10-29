af8f852d90688eb33fae1cc0cc3e95f45cb00e5c
Aleksandr <alexchembarov@gmail.com>
2022-10-29 16:02:27 +0400

    puzirSort

---
+++ puzirSort/Program.cs
@@ -0,0 +1,13 @@
+﻿
+int[] array = { 3, 1, 5, 0, 7, 9, 8 };
+Console.WriteLine("Initial Massive [ " + string.Join(" , ", array) + " ]");
+
+for (int i = 0; i < array.Length; i++)
+{
+    for (int j = 0; j < array.Length - 1; j++)
+    {
+        if(array[j] > array[j+1])
+        (array[j],array[j+1]) = (array[j+1],array[j]);
+    }
+}
+Console.WriteLine("Sort Massive [ " + string.Join(" , ", array) + " ]");
