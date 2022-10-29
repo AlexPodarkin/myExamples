297708173c105c3fcada794091a7978cdda06b97
Aleksandr <alexchembarov@gmail.com>
2022-10-29 13:32:19 +0400

    viborSort

---
+++ sortString/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+string[] array = new string[5]; // razmer massiva 
+for (int i = 0; i < 5; i++)
+{
+    Console.Write("Vvedite stroku:");
+    array[i] = Console.ReadLine(); // Zapolnenie strokami
+}
+Console.WriteLine("Nachalo [" + string.Join(", ", array) + "]");
+for (int i = 0; i < 4; i++) //razmer massiva -1 
+{
+    int MinIndex = i;
+    for (int j = i + 1; j < array.Length; j++)
+    {
+        if (array[MinIndex].Length > array[j].Length)
+            MinIndex = j;
+    }
+    (array[i], array[MinIndex]) = (array[MinIndex], array[i]);
+}
+Console.WriteLine("Sort [" + string.Join(", ", array) + "]");
--- viborSort/Program.cs
+++ viborSort/Program.cs
@@ -19,7 +19,7 @@ int[] SortArray(int[] array)
     for (int i = 0; i < array.Length - 1; i++)
     {
         int min = array[i];
-        for (int j = i; j < array.Length; j++)
+        for (int j = i + 1; j < array.Length; j++)
         {
             if (min > array[j])
             {
@@ -47,4 +47,36 @@ SortArray(array);
 Console.WriteLine("Отсортированный мвссив:");
 PrintArray(array);
 
+
+void SortButckamp()
+{
+    Console.Write("Введите кол-во элементов массива:");
+    int n = Convert.ToInt32(Console.ReadLine());
+    // Заполнение массива
+    int[] array = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        Console.Write("Введите число: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    Console.WriteLine();
+    Console.WriteLine("Начальный массив: [" + string.Join(", ", array) + "]");
+    // Cортировка
+    for (int i = 0; i < n - 1; i++)
+    {
+        int MinIndex = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (array[j] < array[MinIndex])
+                MinIndex = j;
+        }
+        (array[MinIndex], array[i]) = (array[i], array[MinIndex]); /* int temp;
+                                                               temp = array[MinIndex];
+                                                               array[MinIndex] = array[i];
+                                                               array[i] = temp; */
+    }
+    Console.WriteLine("Конечный массив: [" + string.Join(", ", array) + "]");
+}
+
+SortButckamp();
 
